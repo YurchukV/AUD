ff99eafa9722ea2a46b9aedbc854b4a71a7eb62c
Viktor Yurchuk <>
2025-03-17 20:27:54 +0500

    no message

--- VND.php
+++ VND.php
@@ -1,4 +1,11 @@
 <?php
-	$feed = file_get_contents('https://www.sbv.gov.vn/TyGia/faces/ReraSbvOc.jspx');
-	file_put_contents("ReraSbvOc.jspx", $feed);
+	$ctx = stream_context_create(array('http'=>
+		array(
+			'timeout' => 200,  //1200 Seconds is 20 Minutes
+		)
+	));
+	$feed = file_get_contents('https://www.sbv.gov.vn/TyGia/faces/ReraSbvOc.jspx', false, $ctx);
+	if !(empty($feed)) {
+		file_put_contents("ReraSbvOc.jspx", $feed);
+	}
 ?>
